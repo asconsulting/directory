0009dcd00373939d26bc7adb79554c20a5c1782f
Andrew Stevens <andrew@andrewstevens.consulting>
2022-10-10 13:47:33 -0400

    Delete Confirm

--- system/modules/asc_directory/dca/tl_asc_directory_section.php
+++ system/modules/asc_directory/dca/tl_asc_directory_section.php
@@ -76,7 +76,7 @@ $GLOBALS['TL_DCA']['tl_asc_directory_section'] = array
                 'label'               => &$GLOBALS['TL_LANG']['tl_asc_directory_section']['delete'],
                 'href'                => 'act=delete',
                 'icon'                => 'delete.gif',
-                'attributes'          => 'onclick="if(!confirm(\'' . $GLOBALS['TL_LANG']['MSC']['deleteConfirm'] . '\'))return false;Backend.getScrollOffset()"'
+                'attributes'          => 'onclick="if(!confirm(\'' . ($GLOBALS['TL_LANG']['MSC']['deleteConfirm'] ?? null) . '\'))return false;Backend.getScrollOffset()"'
             ),
             'toggle' => array
 			(
